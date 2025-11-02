e429885b97d6ed3563a5e08b4eaf3e8d7c8a8be0
Vivek Nagarajan <vivekn@gmail.com>
2025-11-02 15:33:16 -0800

    Server responding with PONG

--- app/main.py
+++ app/main.py
@@ -5,10 +5,9 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    # Uncomment the code below to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    connection, _ = server_socket.accept() # wait for client
+    connection.sendall(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
