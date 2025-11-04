1e0ff453bcd42431bc2188acb1cd9c5c998fb6d9
Vivek Nagarajan <vivekn@gmail.com>
2025-11-03 20:18:15 -0800

    Server responding to multiple PINGs

--- app/main.py
+++ app/main.py
@@ -9,8 +9,8 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    connection, _ = server_socket.accept() # wait for client
     while True:
-        connection, _ = server_socket.accept() # wait for client
         handle_client(connection)
 
 
