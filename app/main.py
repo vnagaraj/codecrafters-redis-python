076780e34891a4516221f42985e4ffec07d89a20
Vivek Nagarajan <vivekn@gmail.com>
2025-11-03 20:19:38 -0800

    Server responding to multiple PINGs

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     connection, _ = server_socket.accept() # wait for client
     while True:
-        data = conn.recv(1024)
+        data = connection.recv(1024)
         if data:
             handle_client(connection)
 
